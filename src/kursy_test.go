a568abf55af94d5c192eb38be2733f1d9490d74c
piotr.jaskulski@gmail.com <piotr.jaskulski@gmail.com>
2020-11-16 16:15:12 +0100

    more tests...

--- src/kursy_test.go
+++ src/kursy_test.go
@@ -5,8 +5,52 @@ import (
 )
 
 func TestCharactersAllowed(t *testing.T) {
-	var text string = "1234-12-11"
-	if !charAllowed(text, false) {
-		t.Errorf("want: %t, got: %t", true, false)
+
+	tests := []struct {
+		name      string
+		text      string
+		dateRange bool
+		want      bool
+	}{
+		{
+			name:      "Poprawna data",
+			text:      "2020-12-11",
+			dateRange: false,
+			want:      true,
+		},
+		{
+			name:      "Niepoprawna data",
+			text:      "2020/11/10",
+			dateRange: false,
+			want:      false,
+		},
+		{
+			name:      "Niepoprawna data 2",
+			text:      "2020:11:10",
+			dateRange: false,
+			want:      false,
+		},
+		{
+			name:      "Poprawny zakres",
+			text:      "2020-11-12:2020-11-13",
+			dateRange: true,
+			want:      true,
+		},
+		{
+			name:      "Niepoprawny zakres",
+			text:      "2020/11/12-2020/11/13",
+			dateRange: true,
+			want:      false,
+		},
 	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := charAllowed(tt.text, tt.dateRange)
+			if result != tt.want {
+				t.Errorf("oczekiwano: %t; otrzymano: %t", tt.want, result)
+			}
+		})
+	}
+
 }
